c617b338971033127da3ada285820015f1467a53
Blake Bourque <techplex.engineer@gmail.com>
2022-07-30 12:42:03 -0400

    Code to convert SVG to SVG with all strokes ready for cutting on epilog laser

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"aqwari.net/xml/xmltree"
 	"bufio"
 	"bytes"
+	"encoding/xml"
+	"flag"
 	"fmt"
 	"io"
+	"io/fs"
+	"io/ioutil"
 	"log"
 	"os"
 	"regexp"
+	"strings"
 )
 
-func run(inFile string) error {
+func run(inFile string, outFile string) error {
 	file, err := os.Open(inFile)
 	if err != nil {
 		return fmt.Errorf("unable to open %s - %w", inFile, err)
@@ -19,25 +25,89 @@ func run(inFile string) error {
 
 	outStream := bytes.Buffer{}
 
-	if err := fixStoke(file, &outStream); err != nil {
+	desiredStrokeWidthIn := .001
+
+	if err := fixStoke(file, &outStream, desiredStrokeWidthIn); err != nil {
 		return fmt.Errorf("unable to fixStroke - %w", err)
 	}
 
-	fmt.Printf("%s", outStream.String())
+	if len(outFile) == 0 {
+		outFile = strings.TrimSuffix(inFile, ".svg") + "-for-laser.svg"
+	}
+
+	err = ioutil.WriteFile(outFile, outStream.Bytes(), fs.ModePerm)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
 
 func main() {
-	file := "./samples/drill_drawer_2/Drill_Drawer_Drawings_2.svg"
+	inFile := flag.String("f", "", "input svg file to convert to pdf for laser")
+	outFile := flag.String("o", "", "output filename, defaults to input file name with -for-laser.svg appended")
+	flag.Parse()
 
-	if err := run(file); err != nil {
+	if err := run(*inFile, *outFile); err != nil {
 		log.Printf("Error: %s", err.Error())
 		os.Exit(1)
 	}
 }
 
-func fixStoke(inStream io.Reader, outStream io.Writer) error {
+func fixStoke(inStream io.Reader, outStream io.Writer, desiredStrokeWidthIn float64) error {
+	file, err := ioutil.ReadAll(inStream)
+	if err != nil {
+		return fmt.Errorf("unable to read file to fix strokes - %w", err)
+	}
+	rootEle, err := xmltree.Parse(file)
+	if err != nil {
+		return fmt.Errorf("unable to parse file to fix strokes - %w", err)
+	}
+
+	if rootEle.StartElement.Name.Local != "svg" {
+		return fmt.Errorf("root element is not svg, it is '%s'", rootEle.StartElement.Name.Local)
+	}
+	attrs := SVGAttrs{
+		width:   rootEle.Attr("", "width"),
+		height:  rootEle.Attr("", "height"),
+		viewbox: rootEle.Attr("", "viewBox"),
+	}
+
+	resPxPerIn, err := attrs.getResolutionPxPerIn()
+	if err != nil {
+		return fmt.Errorf("fixStoke - %w", err)
+	}
+
+	desiredStrokeWidthSVGUnits := desiredStrokeWidthIn * float64(resPxPerIn)
+
+	// find elements with a stroke-width that need to be changed
+	elementsNeedChanges := rootEle.SearchFunc(func(ele *xmltree.Element) bool {
+		for _, attr := range ele.StartElement.Attr {
+			if attr.Name.Local == "stroke-width" {
+				return true
+			}
+		}
+		return false
+	})
+	// change the stroke width
+	for _, ele := range elementsNeedChanges {
+		ele.SetAttr("", "stroke-width", fmt.Sprintf("%.3f", desiredStrokeWidthSVGUnits))
+	}
+
+	// output the resulting file
+	_, err = fmt.Fprintf(outStream, xml.Header)
+	if err != nil {
+		return err
+	}
+	_, err = fmt.Fprintf(outStream, "%s", rootEle)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// this works but it assumes that the documents will always be 300 pixels per inch
+func fixStrokeAssume300PxPerInch(inStream io.Reader, outStream io.Writer) error {
 	re := regexp.MustCompile(`stroke-width="([\d.]+)"`)
 
 	scanner := bufio.NewScanner(inStream)
--- main_test.go
+++ main_test.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"encoding/json"
-	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"os"
 	"testing"
 )
 
@@ -14,90 +12,6 @@ import (
 	"aqwari.net/xml/xmltree"
 	"github.com/matryer/is"
 )
-
-// SVG is a SVG document
-type SVG struct {
-	Width  string `xml:"width,attr"`
-	Height string `xml:"height,attr"`
-	Doc    string `xml:",innerxml"`
-}
-
-// Store unparsed tags in a map (useful with flat dynamic xml).
-// Inspired from: https://stackoverflow.com/questions/30928770/marshall-map-to-xml-in-go/33110881
-// The main difference is that it can be mixed with defined tags in a struct.
-//For each unparsed element UnmarshalXML is called once.
-
-// NOTE: to be used in flat xml part with distinct tag names
-// If it's not flat: the hash will contains key with empty values
-// If there is several tags with the same name : only the last value will be stored
-
-// UnparsedTag contains the tag informations
-type UnparsedTag struct {
-	XMLName xml.Name
-	Content string `xml:",chardata"`
-	//FullContent   string `xml:",innerxml"` // for debug purpose, allow to see what's inside some tags
-}
-
-// UnparsedTags store tags not handled by Unmarshal in a map, it should be labelled with `xml",any"`
-type UnparsedTags map[string]string
-
-func (m *UnparsedTags) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
-	if *m == nil {
-		*m = UnparsedTags{}
-	}
-
-	e := UnparsedTag{}
-	err := d.DecodeElement(&e, &start)
-	if err != nil {
-		return err
-	}
-
-	//if _, ok := (*m)[e.XMLName.Local]; ok {
-	//	return fmt.Errorf("UnparsedTags: UnmarshalXML: Tag %s:  multiple entries with the same name", e.XMLName.Local)
-	//}
-	(*m)[e.XMLName.Local] = e.Content
-
-	return nil
-}
-
-func (u *UnparsedTags) GetContentByName(name string) string {
-	return ((map[string]string)(*u))[name]
-}
-
-func Test_customDecoder(t *testing.T) {
-	is := is.New(t)
-
-	f, err := os.Open("./samples/drill_drawer_2/Drill_Drawer_Drawings_2.svg")
-	is.NoErr(err)
-	defer f.Close()
-
-	elements := make([]UnparsedTag, 0)
-
-	if err := xml.NewDecoder(f).Decode(&elements); err != nil {
-		is.NoErr(err)
-	}
-
-	log.Printf("%#v", elements)
-}
-
-func TestGetResolution(t *testing.T) {
-
-	attrs := SVGAttrs{
-		width:   "457.2mm",
-		height:  "457.2mm",
-		viewbox: "0 0 5400 5400",
-	}
-	attrs.getResolutionPxPerIn()
-}
-
-//func Contains[T comparable](s []T, e T) bool {
-//	for _, v := range s {
-//		if v == e {
-//			return true
-//		}
-//	}
-//	return false
-//}
 
 func Test_xmltree(t *testing.T) {
 	is := is.New(t)
@@ -108,10 +22,7 @@ func Test_xmltree(t *testing.T) {
 	rootEle, err := xmltree.Parse(file)
 	is.NoErr(err)
 
-	//indent, err := json.MarshalIndent(rootEle, "", "    ")
-	//is.NoErr(err)
-	//
-	//log.Printf("tree: %s", indent) // this might work!!! Ignore content
+	log.Printf("Scope %s", rootEle.Scope)
 
 	toJson := func(ele interface{}) string {
 		indent, err := json.MarshalIndent(ele, "", "    ")
@@ -120,9 +31,6 @@ func Test_xmltree(t *testing.T) {
 	}
 	_ = toJson
 
-	//width := -1
-	//height := -1
-	//log.Printf("StartElement %#v", rootEle.StartElement)
 	if rootEle.StartElement.Name.Local != "svg" {
 		return // fmt.Errorf("root element is not svg")
 	}
@@ -131,14 +39,12 @@ func Test_xmltree(t *testing.T) {
 		height:  rootEle.Attr("", "height"),
 		viewbox: rootEle.Attr("", "viewBox"),
 	}
-	//log.Printf("%s", toJson(rootEle.StartElement.Attr))
+
 	resPxPerIn, err := attrs.getResolutionPxPerIn()
 	is.NoErr(err)
-	//log.Printf("%v", resPxPerIn)
 
 	desiredStrokeWidthIn := .001
 	desiredStrokeWidthSVGUnits := desiredStrokeWidthIn * float64(resPxPerIn)
-	//log.Printf("%f", desiredStrokeWidthSVGUnits)
 
 	elementsNeedChanges := rootEle.SearchFunc(func(ele *xmltree.Element) bool {
 		for _, attr := range ele.StartElement.Attr {
@@ -154,98 +60,3 @@ func Test_xmltree(t *testing.T) {
 
 	log.Printf("%s", rootEle)
 }
-
-func Test_DFS(t *testing.T) {
-	is := is.New(t)
-
-	data := `
-<first>
-	<second_1>
-		<third_1_1></third_1_1>
-		<third_1_2></third_1_2>
-		<third_1_3></third_1_3>
-	</second_1>
-	<second_2>
-		<third></third>
-	</second_2>
-</first>`
-
-	root, err := xmltree.Parse([]byte(data))
-	is.NoErr(err)
-
-	visitedElements := make([]*xmltree.Element, 0)
-
-	DFS(root, func(ele *xmltree.Element) {
-		visitedElements = append(visitedElements, ele)
-	})
-
-	expectedNames := []string{
-		"first",
-		"second_2",
-		"third_1_3",
-		"third_1_3",
-		"third_1_3",
-		"second_2",
-		"third",
-	}
-
-	is.Equal(len(visitedElements), len(expectedNames))
-
-	for idx, name := range expectedNames {
-		is.Equal(visitedElements[idx].Name.Local, name)
-	}
-}
-
-func DFS(rootElement *xmltree.Element, visit func(ele *xmltree.Element)) {
-	visit(rootElement)
-	if len(rootElement.Children) == 0 {
-		return // no more work to do
-	}
-	for _, ele := range rootElement.Children {
-		DFS(&ele, visit)
-	}
-}
-
-func Test_fixStoke(t *testing.T) {
-	//type args struct {
-	//	inStream io.Reader
-	//}
-	//tests := []struct {
-	//	name          string
-	//	args          args
-	//	wantOutStream string
-	//	wantErr       bool
-	//}{
-	//	// TODO: Add test cases.
-	//}
-	//for _, tt := range tests {
-	//	t.Run(tt.name, func(t *testing.T) {
-	//		outStream := &bytes.Buffer{}
-	//		err := fixStoke(tt.args.inStream, outStream)
-	//		if (err != nil) != tt.wantErr {
-	//			t.Errorf("fixStoke() error = %v, wantErr %v", err, tt.wantErr)
-	//			return
-	//		}
-	//		if gotOutStream := outStream.String(); gotOutStream != tt.wantOutStream {
-	//			t.Errorf("fixStoke() gotOutStream = %v, want %v", gotOutStream, tt.wantOutStream)
-	//		}
-	//	})
-	//}
-}
-
-//func Test_ps_2_draw2d(t *testing.T) {
-//	is := is.New(t)
-//
-//	src, err := os.OpenFile("samples/ps/Drill Drawer Drawings 1.ps", 0, 0)
-//	is.NoErr(err)
-//	defer src.Close()
-//
-//	bytes, err := ioutil.ReadAll(src)
-//	reader := strings.NewReader(string(bytes))
-//
-//	gfxCtx := draw2dimg.NewGraphicContext()
-//	interpreter := ps.NewInterpreter(gfxCtx)
-//	interpreter.Execute(reader)
-//	//saveToPngFile("result/TestPostscript.png", i)
-//
-//}
