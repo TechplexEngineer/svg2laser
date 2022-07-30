5b209236442b8a683bff23fc65bc315e8a909a4e
Blake Bourque <techplex.engineer@gmail.com>
2022-07-30 15:57:10 -0400

    cleanup

--- inkscape.go
+++ inkscape.go
@@ -9,39 +9,18 @@ import (
 	"os/exec"
 )
 
-// svgConvert uses inkscape to convert to a supported output format.
+// svgConvertBuffer uses inkscape to convert to a supported output format. (pdf default)
 // By default supported output types are: svg, png, ps, eps, pdf, emf, wmf
 // see: https://inkscape.org/doc/inkscape-man.html#export-type-TYPE-TYPE
-//func svgConvert(outputFileName string, inputFileName string) error {
-//	inkscapeExecutable := "inkscape"
-//	if exePath, isSet := os.LookupEnv("SVG2LASER_INKSCAPE_PATH"); isSet {
-//		inkscapeExecutable = exePath
-//	}
-//
-//	//inkscape --export-filename=filename.pdf filename.svg
-//	log.Printf("command: %s %s %s %s", inkscapeExecutable, "--export-filename", outputFileName, inputFileName)
-//	err := sh.Run(inkscapeExecutable, "--export-filename", outputFileName, inputFileName)
-//	if err != nil {
-//		return fmt.Errorf("error running inkscape - %w", err)
-//	}
-//	return nil
-//}
-
 func svgConvertBuffer(inputBytes []byte, outputBuffer io.Writer, stderrBuffer io.Writer) error {
 	inkscapeExecutable := "inkscape"
 	if exePath, isSet := os.LookupEnv("SVG2LASER_INKSCAPE_PATH"); isSet {
 		inkscapeExecutable = exePath
 	}
-	return origPipe(inkscapeExecutable, []string{"--pipe", "--export-filename=-", "--export-type=pdf"}, inputBytes, outputBuffer, stderrBuffer)
+	return execPipe(inkscapeExecutable, []string{"--pipe", "--export-filename=-", "--export-type=pdf"}, inputBytes, outputBuffer, stderrBuffer)
 }
 
-//func check(e error) {
-//	if e != nil {
-//		panic(e)
-//	}
-//}
-
-func origPipe(bin string, args []string, stdin []byte, outputBuffer io.Writer, stderrBuffer io.Writer) error {
+func execPipe(bin string, args []string, stdin []byte, outputBuffer io.Writer, stderrBuffer io.Writer) error {
 
 	cmd := exec.Command(bin, args...)
 	in, err := cmd.StdinPipe()
@@ -93,57 +72,6 @@ func origPipe(bin string, args []string, stdin []byte, outputBuffer io.Writer, s
 	if err = cmd.Wait(); err != nil {
 		if exitError, ok := err.(*exec.ExitError); ok && exitError.ExitCode() != 3 {
 			log.Printf("ERROR: %s - %s\n", stderrB, err)
-			if err != nil {
-				return err
-			}
-		}
-	}
-
-	return nil
-}
-
-func brokenPipe(bin string, args []string, inputBytes []byte, outputBuffer io.Writer, stderrBuffer io.Writer) error {
-	cmd := exec.Command(bin, args...)
-	in, err := cmd.StdinPipe()
-	if err != nil {
-		return err
-	}
-	defer in.Close()
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return err
-	}
-	defer stdout.Close()
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		return err
-	}
-	defer stderr.Close()
-	err = cmd.Start()
-	if err != nil {
-		return err
-	}
-
-	_, err = in.Write(inputBytes)
-	if err != nil {
-		return err
-	}
-
-	fileBytes, err := ioutil.ReadAll(stdout)
-	_ = fileBytes
-	_, err = io.Copy(outputBuffer, stdout)
-	if err != nil {
-		return err
-	}
-	if stderrBuffer != nil {
-		_, err = io.Copy(stderrBuffer, stderr)
-		if err != nil {
-			return err
-		}
-	}
-
-	if err = cmd.Wait(); err != nil {
-		if exitError, ok := err.(*exec.ExitError); ok && exitError.ExitCode() != 3 {
 			if err != nil {
 				return err
 			}
